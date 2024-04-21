08177719d936b6f3fd80df8ffad7618103a2b803
Daniel Asare <dannyasare29@gmail.com>
2024-04-21 20:47:45 +0000

    Add Up

--- fundamentals.js
+++ fundamentals.js
@@ -97,25 +97,47 @@ if (age >= 18){
   }
   
   console.log(century);
-  */
-
+  
   // type conversion 
-
+  
   const inputYear = '1996';
   console.log(Number(inputYear), inputYear);
   console.log(Number(inputYear) + 18);
 
   console.log(Number('Daniel'));
   console.log(typeof NaN);
-
+  
   console.log(String(23), 23);
-
+  
   // Type coercion
   console.log('I am ' + 23 + 'years old');
   console.log('23' - '10' - 5);
   console.log('23' / '2');
-
+  
   let n = '1' + 1;
   n -= 1;
   console.log(n);
+  */
 
+  // 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Daniel'));
+console.log(Boolean(''));
+console.log(Boolean({}));
+
+const money = 0;
+
+if (money){
+  console.log('Dont spend it all');
+} else {
+  console.log('You should get a job');
+}
+
+let height;
+if (height){
+  console.log('Yay!, Height is defined');
+} else {
+  console.log(' Height is UNDEFINED');
+}
